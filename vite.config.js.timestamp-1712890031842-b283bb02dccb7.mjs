5d7e1758ae914ba8232c45034f79df85a317ff17
nedmad <matheusmned@gmail.com>
2024-04-20 10:46:02 -0300

    ne

--- vite.config.js.timestamp-1712890031842-b283bb02dccb7.mjs
+++ vite.config.js.timestamp-1712890031842-b283bb02dccb7.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from "file:///C:/Users/mned2/OneDrive/Documentos/react/infolibre/node_modules/vite/dist/node/index.js";
 import react from "file:///C:/Users/mned2/OneDrive/Documentos/react/infolibre/node_modules/@vitejs/plugin-react/dist/index.mjs";
 var vite_config_default = defineConfig({
-  base: '/infolibre/',
   plugins: [react()]
 });
 export {
