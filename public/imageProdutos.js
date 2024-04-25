097c88ac8d370518c07f84ae1413d7916fe5f1b5
nedmad <matheusmned@gmail.com>
2024-04-25 17:55:10 -0300

    finalizado a edicao de produtos

--- public/imageProdutos.js
+++
@@ -1,2 +0,0 @@
-//detox
-export {default as detox1} from "./images/produto detox/lift_detox_principal.webp";
--- src/components/Infoproduto.jsx
+++ src/components/Infoproduto.jsx
@@ -2,7 +2,7 @@ import produtos from "../json/Produtos.json";
 import Carts from "../objects/Carts";
 
 export default function Inforodutos() {
-  const produto = produtos.filter((val) => val.tipo === "info");
+  const produto = produtos.filter((val) => val.tipo === "infoproduto");
 
   return (
     <>
